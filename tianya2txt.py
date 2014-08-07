d6e1eb294067452c561d55013cc822d84768d22c
limuxy <limuxy@gmail.com>
2014-08-07 18:03:09 +0800

    Correct problem on duplicate contents

--- tianya2txt.py
+++ tianya2txt.py
@@ -17,6 +17,7 @@ def get_soup(url):
     return soup
 
 def get_post_info(url):
+    url = url = re.sub(r'-\d+.shtml', '-1.shtml', url)
     soup = get_soup(url)
     #get post subject as file_name
     subject = soup.find('span', {'class':'s_title'}).findChildren('span')[0].text
@@ -27,33 +28,31 @@ def get_post_info(url):
     file_name = file_name.replace('\\','')
     file_name = file_name.replace(':','')
     #get page count from navigation form
-    form = soup.find('form')
-    if form == None:
+    forms = soup.find_all('form')
+    if len(forms) == 1:
         page_amount = 1
     else:
-        form_a = form.findChildren('a')[-4]
+        form_a = forms[1].findChildren('a')[-2]
         page_amount = int(form_a.text)
     return (file_name, page_amount)
 
 def get_page_content(params):
     (url, page) = params
+    url = re.sub(r'-\d+.shtml', '-'+str(page)+'.shtml', url)
     print u'正在获取第{}页'.format(page)
     page_content = ''
-    url = re.sub(r'-\d+.shtml', '-'+str(page)+'.shtml', url)
     soup = get_soup(url)
-    posts = soup.find_all('div', class_='bbs-content')
-    if posts != None and posts != []:
-        reply_id = int(soup.find_all('div', class_="atl-item")[-1]['id'])
-        for post in posts:
+    items = soup.find_all('div', class_='atl-item')
+    reply_id = int(items[-1]['id'])
+    for item in items:
+        if item.findChildren('div', class_='atl-head') == [] or item.findChildren('strong', class_='host') != []:
+            post = item.findChildren('div', class_='bbs-content')[0]
             post_content =  post.text
             if re.search(r'\d+-\d+-\d+\s\d+:\d+:\d+', post_content) != None:
                 continue
             post_content = re.sub(r'\t+|(<br>)+|(<\/br>)+', '', post_content)
             post_content = re.sub(u'\u3000+', "\n", post_content)
             page_content += post_content
-    else:
-        reply_id = 0
-        page_content = ''
     return (page_content, reply_id)
 
 
@@ -68,19 +67,19 @@ if __name__ == '__main__':
     -------------------- ----------------
     '''
     print u"请输入天涯帖子的地址："
-    tianya_url = raw_input()
+    url = raw_input()
     #test if input URL is correct
-    while re.match(r'http:\/\/bbs.tianya.cn\/post-[0-9a-zA-Z]+-\d+-\d+.shtml', tianya_url) == None:
+    while re.match(r'http:\/\/bbs.tianya.cn\/post-[0-9a-zA-Z]+-\d+-\d+.shtml', url) == None:
         print u"帖子地址不正确，重新输入："
-        tianya_url = raw_input()
-    url = "http://bbs.otianya.cn/cgi-bin/bbs.pl?url="+tianya_url
+        url = raw_input()
 
     page = int(re.search(r'-(\d+).shtml', url).group(1))
-    file_name, page_amount = get_post_info(url)
+    file_name, page_amount= get_post_info(url)
     print u"共{}页，将从第{}页开始获取".format(page_amount, page)
 
 
     post_content = ''
+
     pool = Pool(processes = 4)
     params = [(url, page) for page in range(page, page_amount+1)]
     post_result= pool.map(get_page_content, params)
@@ -91,6 +90,7 @@ if __name__ == '__main__':
         latest_reply_result.append(latest_reply)
     pool.close()
     pool.join()
+
     post_content_result = ''.join(post_content_result)
     latest_reply_result = max(latest_reply_result)
     latest_mark = u"已获取到最新楼层数：{}".format(latest_reply_result)
